444f9325567a87cae82b532b193ac36f1f5dbd6f
Jarvid <matheemooksombut@gmail.com>
2025-12-09 01:40:39 +0700

    2

--- server.js
+++ server.js
@@ -1,6 +1,13 @@
 const express = require('express')
 const app = express()
+const bodyparser = require('body-parser')
+const mysql = require('mysql2/promise')
+const cors = require('cors')
 const port= 3000; 
+
+
+
+app.use(cors())
 
 app.get('/', (req, res) => {
   res.send('Server is running!')
