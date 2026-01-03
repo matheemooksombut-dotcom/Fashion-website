39474448040bfac9eaeac1fd37c0ca680708a110
Jarvid <matheemooksombut@gmail.com>
2026-01-03 20:54:47 +0700

    efs

--- fontscript.js
+++ fontscript.js
@@ -4,6 +4,23 @@
 
 
 
+// Register     Dom
+function RegisterDom(){
+   
+   
+
+    let Username  = document.querySelector('.Username').value
+    let password  = document.querySelector('.Password').value
+    let confrimePassword  = document.querySelector('.ConfrimePassword').value
+    let Firstname  = document.querySelector('.Firstname').value
+    let Lastname  = document.querySelector('.Lastname').value
+
+   
+    
+}
+
+
+
 
 
 
