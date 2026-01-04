dd513421c27cd1917e2d0f3c1cced832accba5e2
Jarvid <matheemooksombut@gmail.com>
2026-01-04 23:45:30 +0700

    s

--- fontscript.js
+++ fontscript.js
@@ -2,8 +2,21 @@
 
 
 // Register     Dom
+// ดึงค่าจาก input
+function RegisterDom() {
+  return {
+    Username: document.querySelector('.Username').value,
+    Password: document.querySelector('.Password').value,
+    ConfrimePassword: document.querySelector('.ConfrimePassword').value,
+    Firstname: document.querySelector('.Firstname').value,
+    Lastname: document.querySelector('.Lastname').value
+  }
+}
+
+// submit form
 const submitData = async (e) => {
   e.preventDefault()
+
   const data = RegisterDom()
 
   if (data.Password !== data.ConfrimePassword) {
@@ -18,15 +31,19 @@ const submitData = async (e) => {
     Lastname: data.Lastname
   }
 
-  try{
-    await axios.post('http://localhost:3000/adduser', payload)
+  try {
+    await axios.post('http://localhost:3000/adduser', payload, {
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    })
+
     alert('สมัครสมาชิกสำเร็จ')
-
-
     e.target.reset()
-  }catch(err){
+
+  } catch (err) {
     console.error(err)
     alert('ส่งข้อมูลไม่สำเร็จ')
   }
-  
 }
+
